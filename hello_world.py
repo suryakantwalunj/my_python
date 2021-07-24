c60ae761b6442c30b3320f20c03f323e9078f9fa
suryakantwalunj <suryakantswalunj@gmail.com>
2021-07-24 18:18:25 +0530

    python data structure
    print python data structure.

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+print("Hello World")
+# List 
+fruits=["apple","banana","canberry","dyna"]
+# Dictionary
+homes={"101":"Mr. Jagtap", "102":"Mr. Mane","103":"Mr. Kelkar"}
+#  Set in python
+gadgets={"mobile","laptop","smart-watch","mobile"}
+#tuple
+rgb=("red","green","blue")
+print(fruits)
+print(homes)
+print(gadgets)
+print(rgb)
